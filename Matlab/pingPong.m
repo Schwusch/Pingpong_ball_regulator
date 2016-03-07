0fca95cb57c2c729258d3c53df7b4d3e7df0f89a
Jonathan Böcker <jonathan.e.m.bocker@gmail.com>
2016-03-07 01:10:30 +0100

    Update pingPong.m

--- Matlab/pingPong.m
+++ Matlab/pingPong.m
@@ -1,9 +1,9 @@
 % Controller/plotting settings-----------------
-P = 150;
-I = 150000;
-D = 0;
-SETDISTANCE = 200;
-TIMER = 50;
+P = 150;                                        % Values with a factor of 1000
+I = 150000;                                     % --||--
+D = 0;                                          % --||--
+SETDISTANCE = 200;                              % Setpoint in mm from sensor
+TIMER = 50;                                     % Sampling time in ms
 COM_PORT = 'COM6';
 BAUDRATE = 115200;
 A = importdata('regulator4.txt');               %Import conversion array
@@ -24,31 +24,30 @@ pause(1)
 flushoutput(a);                                 %flush serial output/input 
 flushinput(a);                                  %buffer before attempting to send
 index = 1;
-while (index < 101)
+while (index < 101)                             % Transfer conversion array
     str = num2str(A(index));
     fprintf(a, str);
     index = index + 1;
 end
-fprintf(a, num2str(P));
-fprintf(a, num2str(I));
-fprintf(a, num2str(D));
-fprintf(a, num2str(SETDISTANCE));
-fprintf(a, num2str(TIMER));
-while ishandle(plotGraph)    
+fprintf(a, num2str(P));                         % Transfer controller values
+fprintf(a, num2str(I));                         % --||--
+fprintf(a, num2str(D));                         % --||--
+fprintf(a, num2str(SETDISTANCE));               % --||--
+fprintf(a, num2str(TIMER));                     % --||--
+while ishandle(plotGraph)                       % While plotting window is active
     index = 1;  
-    while(index < freq + 1)                     %read and insert values to temp matrix
+    while(index < freq + 1)                     % Read and insert values to temp matrix
         tempValues(1,1) = str2double(fscanf(a,'%c'));   
         tempValues(1,2) = str2double(fscanf(a,'%c'));
         tempValues(1,3) = str2double(fscanf(a,'%c'));        
-        tempValues = circshift(tempValues, 1);        
+        tempValues = circshift(tempValues, 1);  % Shift values downwards
         index = index + 1;        
     end
-    values = circshift(values, freq);    
-    values(1:freq,1:3) = tempValues;
-    
-    plotGraph = plot(x,values);
+    values = circshift(values, freq);           % Shift big matrix to fit temporary matrix
+    values(1:freq,1:3) = tempValues;            % Insert temp matrix to plot latest values
+    plotGraph = plot(x,values);                 % Plot latest values
     legend('Distance', 'error', 'output', 'setpoint');
-    pause(0.5)
+    pause(0.5)                                  % Pause to let Matlab render
 end
 fclose(a);
 plot(x,values);
